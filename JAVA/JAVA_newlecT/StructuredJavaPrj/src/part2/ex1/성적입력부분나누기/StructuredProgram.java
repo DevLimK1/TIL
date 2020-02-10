71ba1f5cdb11bdd9cf001c620d1bf01533cb0153
DevLimK1 <49403147+DevLimK1@users.noreply.github.com>
2020-02-10 13:15:17 +0900

    Bottom up 구조 완료

--- JAVA/JAVA_newlecT/StructuredJavaPrj/src/part2/ex1/성적입력부분나누기/StructuredProgram.java
+++ JAVA/JAVA_newlecT/StructuredJavaPrj/src/part2/ex1/성적입력부분나누기/StructuredProgram.java
@@ -6,60 +6,34 @@ import java.util.Scanner;
 public class StructuredProgram {
 	
  
-	/*
-	 * 전역 변수 만들어서 main에 있었던 변수를 
-	 * 성적입력 함수에도 변수를 공유해서 사용 가능
-	 */	
-	static int[] kors = new int[3]; 
+	
 
 	public static void main(String[] args) {
+		
+		/*
+		 * 전역변수->지역변수
+		 * 성적입력, 성적출력 함수의 매개변수로 넘겨줌
+		 */
+		int[] korList = new int[3]; 
 
-		int total = 0;
-		float avg; // 평균
+		
 		int menu;
 		boolean keepLoop = true;
 
-		Scanner scan = new Scanner(System.in);
-
 		while (keepLoop) {
-			// 메인 메뉴부분
-			System.out.println("┌────────────────────┐");
-			System.out.println("│      메인 메뉴     │");
-			System.out.println("└────────────────────┘");
-			System.out.println("\t1.성적입력");
-			System.out.println("\t2.성적출력");
-			System.out.println("\t3.종료");
-			System.out.print("\t선택>");
-			menu = scan.nextInt();
-
-			// 성적 입력부분
+			
+			menu=inputMenu();
+		
 			switch (menu) {
 
 			case 1:
 				
-				성적입력();
+				inputKors(korList);
 				
 				break;
 
 			case 2:
-				// 성적 출력부분
-				for (int i = 0; i < 3; i++)
-					total += kors[i];
-
-				avg = total / 3.0f;
-
-				System.out.println("┌────────────────────┐");
-				System.out.println("│      성적 출력     │");
-				System.out.println("└────────────────────┘");
-				System.out.println();
-
-				for (int i = 0; i < 3; i++)
-					System.out.printf("국어%d : %3d\n", 3 - i, kors[i]);
-
-				System.out.printf("총점 :%3d\n", total);
-				System.out.printf("평균 :%6.2f\n", avg);
-				System.out.println("───────────────────────────────────────────────────────");
-
+				printKors(korList);
 				break;
 
 			case 3:
@@ -73,24 +47,74 @@ public class StructuredProgram {
 		}
 	} //~main
 	
+	
+	static int inputMenu() {
+		
+		Scanner scan = new Scanner(System.in);
+		// 메인 메뉴부분
+		System.out.println("┌────────────────────┐");
+		System.out.println("│      메인 메뉴     │");
+		System.out.println("└────────────────────┘");
+		System.out.println("\t1.성적입력");
+		System.out.println("\t2.성적출력");
+		System.out.println("\t3.종료");
+		System.out.print("\t선택>");
+		int menu = scan.nextInt();
+		
+		return menu;
+	}
+	
+	/*
+	 * switch 문 case 2에 있었던 코드를 함수로 분리시킴
+	 */
+	static void printKors(int[] kors) {
+		// 성적 출력부분
+		
+		int total = 0;
+		float avg; // 평균
+		
+		for (int i = 0; i < 3; i++)
+			total += kors[i];
+
+		avg = total / 3.0f;
+
+		System.out.println("┌────────────────────┐");
+		System.out.println("│      성적 출력     │");
+		System.out.println("└────────────────────┘");
+		System.out.println();
+
+		for (int i = 0; i < 3; i++)
+			System.out.printf("국어%d : %3d\n", 3 - i, kors[i]);
+
+		System.out.printf("총점 :%3d\n", total);
+		System.out.printf("평균 :%6.2f\n", avg);
+		System.out.println("───────────────────────────────────────────────────────");
+
+	}
+	
 	/*
 	 * switch 문 case 1에 있었던 코드를 함수로 분리시킴
 	 */
-	static void 성적입력() { 
+	static void inputKors(int[] kors) { 
 		Scanner scan = new Scanner(System.in);
+		int kor;
+		
 		System.out.println("┌────────────────────┐");
 		System.out.println("│      성적 입력     │");
 		System.out.println("└────────────────────┘");
 		System.out.println();
 
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < 3; i++) {
 			do {
 				System.out.printf("국어%d: ", i + 1);
-				kors[i] = scan.nextInt();
+				kor= scan.nextInt();
 
-				if (kors[i] < 0 || 100 < kors[i])
+				if (kor< 0 || 100 < kor)
 					System.out.println("국어 성적은 0~100까지 범위 입력만 가능합니다.");
-			} while (kors[i] < 0 || 100 < kors[i]);
+			} while (kor < 0 || 100 < kor);
+			
+			kors[i]=kor;
+		}
 		System.out.println("───────────────────────────────────────────────────────");
 	}
 	
